0cdbc87632d68b58c8fbcd6113d47e6df3d83381
Rodro-Dev <177986121+Rodro-Dev@users.noreply.github.com>
2026-08-21 16:56:51 -0300

    Adding_missing_code

--- data/jokers/monopoly.lua
+++ data/jokers/monopoly.lua
@@ -1,19 +1,59 @@
-SMODS.Joker{
-    key = "monopoly_joker",
+SMODS.Joker {
+    key = 'monopoly_joker',
     loc_txt = {
-        name = "Monopoly Guy",
+        name = 'Monopoly Guy',
         text = {
-            "Te da {C:money}dinero{}"
+            'Earn {C:money}$#1#{} at the end of each round',
+            'and reduce this amount by {C:money}$5{}',
+            'Like {C:blue}Ice Cream{} but with money.'
         }
     },
     rarity = 3,
-    cost = 8,
-    atlas = "monopoly_joker",
-    pos = {x = 0, y = 0},
+    cost = 12,
+    unlocked = true,
+    discovered = false,
+    blueprint_compat = false,
+    brainstorm_compat = false,
+    atlas = 'monopoly_joker',
+    pos = { x = 0, y = 0 },
 
-    calculate = function(self,card,context)
-        if context.joker_main then
-            return {}
+    config = {
+        extra = {
+            dollars = 100,
+            decrease = 5
+        }
+    },
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.dollars
+            }
+        }
+    end,
+
+    calculate = function(self, card, context)
+
+        if context.end_of_round and context.main_eval then
+
+            local payout = card.ability.extra.dollars
+
+            -- Give money
+            if payout > 0 then
+                card.ability.extra.dollars =
+                    card.ability.extra.dollars - card.ability.extra.decrease
+
+                -- Destroy after the final payout
+                if card.ability.extra.dollars <= 0 then
+                    card:start_dissolve()
+                end
+
+                return {
+                    dollars = payout,
+                    message = '$' .. payout,
+                    colour = G.C.MONEY
+                }
+            end
         end
     end
 }
