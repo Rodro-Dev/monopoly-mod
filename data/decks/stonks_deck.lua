a4ac7b9c45f9ecbabcd3bc96bb964afc75920ca6
Rodro-Dev <rodrigolucasricca35@gmail.com>
2026-09-14 13:51:30 -0300

    change stonks deck description and structure

--- data/decks/stonks_deck.lua
+++ data/decks/stonks_deck.lua
@@ -1,59 +1,54 @@
+
 SMODS.Back {
-    key = "stonks_deck",
-
-    loc_txt = {
-        name = "Stonks Deck",
-        text = {
-            "Start with {C:money}$10{}",
-            "Start with {C:money}Seed Money{}",
-            "Start with a {C:purple}Negative{} and {C:purple}Eternal{}",
-            "{C:attention}To the Moon{}",
-            "Start with {C:red}1 less{} {C:blue}hand{}"
-        }
-    },
-
-    atlas = "atlas_deck",
+    key = 'stonks_deck',
     pos = { x = 0, y = 0 },
-
+    config = {
+        vouchers = { "v_seed_money" },
+    },
+    loc_txt = {
+        name = 'Stonks Deck',
+        text = {
+            [1] = 'Start with {C:money}$10{}',
+            [2] = 'Start with {C:money}Seed Money{}',
+            [3] = 'Add a {C:dark_edition}Negative{} {C:purple}Eternal{}',
+            [4] = '{C:attention}To the Moon{} each Ante',
+            [5] = 'Start with {C:attention}1{} less {C:blue}hand{}',
+            [6] = 'Start with {C:attention}1{} less {C:red}discard{}'
+        },
+    },
     unlocked = true,
     discovered = true,
-
-    config = {
-        vouchers = {
-            "v_seed_money"
-        }
-    },
-
+    no_collection = false,
+    atlas = 'atlas_deck',
+    calculate = function(self, card, context)
+        if context.end_of_round and context.main_eval and G.GAME.blind.boss then
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    play_sound('timpani')
+                    local new_joker = SMODS.add_card({ set = 'Joker', key = 'j_to_the_moon' })
+                    if new_joker then
+                        new_joker:set_edition("e_negative", true)
+                        new_joker:add_sticker('eternal', true)
+                    end
+                    return true
+                end
+            }))
+        end
+    end,
     apply = function(self, back)
-        G.GAME.starting_params.dollars = 10
-
-        G.GAME.starting_params.hands =
-            G.GAME.starting_params.hands - 1
-
         G.E_MANAGER:add_event(Event({
             func = function()
-                local card = create_card(
-                    "Joker",
-                    G.jokers,
-                    nil,
-                    nil,
-                    nil,
-                    nil,
-                    "j_to_the_moon"
-                )
-
-                card:add_to_deck()
-
-                card:set_edition({
-                    negative = true
-                }, true)
-
-                card.ability.eternal = true
-
-                G.jokers:emplace(card)
-
+                play_sound('timpani')
+                local new_joker = SMODS.add_card({ set = 'Joker', key = 'j_to_the_moon' })
+                if new_joker then
+                    new_joker:set_edition("e_negative", true)
+                    new_joker:add_sticker('eternal', true)
+                end
                 return true
             end
         }))
+        G.GAME.starting_params.hands = G.GAME.starting_params.hands - 1
+        G.GAME.starting_params.discards = G.GAME.starting_params.discards - 1
+        G.GAME.starting_params.dollars = 10
     end
 }
